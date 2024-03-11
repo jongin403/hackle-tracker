9fe7d1909df40e435d2f04aa461ef4c281f41179
jongin403 <jongin403@naver.com>
2024-03-11 20:53:19 +0900

    feat: EventSequence 컴포넌트 데이터 추가

--- src/components/EventSequence/index.tsx
+++ src/components/EventSequence/index.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react';
 import styles from './EventSequence.module.scss';
 import classNames from 'classnames/bind';
+import { EventItemOverview } from '../../types/eventItem';
 
 const cx = classNames.bind(styles);
 
 interface EventSequenceProps {
   className?: string;
+  eventItemList?: EventItemOverview[];
 }
 
-const EventSequence: React.FC<EventSequenceProps> = ({ className }) => {
+const EventSequence: React.FC<EventSequenceProps> = ({
+  className,
+  eventItemList = [],
+}) => {
   return (
     <div className={cx(styles.eventSequence, className)}>
-      <div className={styles.eventSequence}>111Header Component</div>
+      <div className={styles.EventItemList}>
+        {eventItemList.map((eventItem) => {
+          return (
+            <div className={styles.eventItem}>
+              <div className={styles.dateTime}>{eventItem.dateTime}</div>
+              <div className={styles.eventName}>{eventItem.eventName}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
